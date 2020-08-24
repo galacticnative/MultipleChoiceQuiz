19d487e09439e00a291d56368229acecfd84ace4
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-08-23 18:58:13 -0700

    re-organized questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,81 @@
 
-var time = 150;
 var score = 0;
 
 var timerEl = document.querySelector("#timer");
 var startBtnEl = document.querySelector("#btn-start");
 var questionListEl = document.querySelector("#question-list");
+var questionContainer = document.querySelector("question-container");
 
 
 var questions = [ 
 {
     question: "This is question 1. What does CSS stand for?",
-    option: "A. Computer Sheet Style",
+    options: ["A. Computer Sheet Style", "B. Cascading Style Sheets", "C. Crystal Style Sheet", "D. Cascading Sheet Style"],
     answer: "B. Cascading Style Sheets",
-    option: "C. Crystal Style Sheet",
-    option: "D. Cascading Sheet Style",
 },
 {
     question: "This is Question 2.",
+    options: ["A. some text", "B. some text", "C. some text", "D. some text"],
     answer: "A. some text",
-    option: "B. some text",
-    option: "C. some text",
-    option: "D. some text",
 },
 {
     question: "This is Question 3.",
-    option: "A. some text",
-    option: "B. some text",
-    option: "C. some text",
+    options: ["A. some text", "B. some text", "C. some text", "D. some text"],
     answer: "D. some text",
 },
 {
     question: "This is Question 4.",
-    option: "A. some text",
-    option: "B. some text",
+    options: ["A. some text", "B. some text", "C. some text", "D. some text"],
     answer: "C. some text",
-    option: "D. some text",
 },
 {
     question: "This is Question 5.",
-    option: "A. some text",
-    option: "B. some text",
-    option: "C. some text",
+    options: ["A. some text", "B. some text", "C. some text", "D. some text"],
     answer: "D. some text",
 },
 ];
 
 var startQuiz = function() {
-    
+
+    countdown();
+    displayQuestion();
+}
+
+var displayQuestion = function() {
     for (var i = 0; i < questions.length; i++) {
+        
         var listEl = document.createElement("li");
         var buttonEl = document.createElement("button");
-        listEl.textContent = questions[i];
+        buttonEl.textContent = questions[i];
         listEl.appendChild(buttonEl);
-        buttonEl.appendChild(questionListEl);
+        questionListEl.appendChild(listEl);    
+        buttonEl.onclick = nextQuestion   
 
         if (questions[i].option === true) {
-            score--;
             questions++;
             alert("Wrong!");
-        } else (questions[i].answer === true) {
+        } else if (questions[i].answer === true) {
             score++;
             questions++;
             alert("Correct!");
         }
-        
-    }
-    timer();
-}
-
-var timer = function() {
-    time--;
-    timerEl.textContent = time
-    if (time === 0) {
-        clearInterval(timerStart);
     }
 }
-var timerStart = setInterval(timer, 1000);
 
-startBtnEl.addEventListener("click", startQuiz());
+var countdown = function() {
+    timeLeft = 75;
+
+    var timer = setInterval(function() {
+        if (timeLeft >= 1) {
+            document.body.querySelector("#timer").textContent = timeLeft;
+            timeLeft--;
+        } else {
+            timerEl.textContent = ''
+            clearInterval(timer);
+            //create end quiz function and to display and enter score with initials
+        }
+    }, 1000);
+
+}
+
+startBtnEl.onclick = startQuiz;
